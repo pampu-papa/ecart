c04aff285e76be60eba3a3c775489dd0df6fe06a
pampu-papa <nroy9july@gmail.com>
2022-07-25 21:52:29 +0530

    5 committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:'',loadChildren: () =>
+  import('./pages/home/home.module').then(m=>m.HomeModule )
+},{
+  path:'product',loadChildren:()=>
+  import('./pages/products/products.module').then(m=>m.ProductsModule)
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
